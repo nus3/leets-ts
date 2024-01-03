e27f1dd06d58cf5f96b071b15c958e6fcfd62cf7
nus3 <hadayota33@gmail.com>
2024-01-03 11:41:01 +0900

    add another solution

--- problems/two_sum.ts
+++ problems/two_sum.ts
@@ -18,10 +18,23 @@ function twoSum(nums: number[], target: number): number[] {
   return results;
 }
 
+function twoSum1(nums: number[], target: number): number[] {
+  const numberMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const difference = target - num;
+    if (numberMap.has(difference)) return [numberMap.get(difference), i];
+    numberMap.set(num, i);
+  }
+
+  return [];
+}
+
 const main = () => {
-  console.log(twoSum([2, 7, 11, 15], 9));
-  console.log(twoSum([3, 2, 4], 6));
-  console.log(twoSum([3, 3], 6));
+  console.log(twoSum1([2, 7, 11, 15], 9));
+  console.log(twoSum1([3, 2, 4], 6));
+  console.log(twoSum1([3, 3], 6));
 };
 
 main();
